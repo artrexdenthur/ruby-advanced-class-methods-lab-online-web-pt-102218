7e65acdc7fd1bb534625b8fb0c7dc701f2414f46
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 01:46:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,26 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    
+  end
 end
