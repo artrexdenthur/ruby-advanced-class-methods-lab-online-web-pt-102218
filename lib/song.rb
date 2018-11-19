1f70a2757f5714b64363821ca1b91b8133b6ca57
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 02:43:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,8 @@ class Song
   def self.new_from_filename(filename)
     filename.delete!('.mp3')
     info = filename.split(' - ')
-    create_by_name(info[1]).artist_name = info[0]
+    create_by_name(info[1]).artist_name = info[1]
+    all.last
   end
     
 end
