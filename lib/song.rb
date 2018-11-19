11c09238f6ea2254f0704762bce0de1a5e474261
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 02:01:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -36,6 +38,6 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    find_by_name(name) || create_by_name(name)
+    find_by_name(name) || binding.pry 
   end
 end
